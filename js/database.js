bcbe5d1e13b2d3519720fc64b3214810e65218b9
Trevor Pierce <trevthewebdev@gmail.com>
2021-10-08 12:39:27 -0500

    updates

--- js/database.js
+++ js/database.js
@@ -1,24 +1,52 @@
 ((app) => {
-  const notifications = [];
+  const notifications = initialNotifications;
 
   /* api calls */
   async function getNotifications() {
-    await sleep(randomInt(2));
+    await sleep(randomNumber(2));
     return {
       data: notifications,
       meta: { total: notifications.length },
     };
   }
 
-  function addNotification(payload) {
-    await sleep(randomInt(2));
+  async function addNotification(payload) {
+    notifications.push({ ...payload, id: notifications.length + 1 });
 
-    notifications.push(payload);
-
+    await sleep(randomNumber(2));
     return { data: payload };
   }
 
   // exports
   app.getNotifications = getNotifications;
   app.addNotification = addNotification;
+
+  window.app = app;
 })(window.app || {});
+
+const initialNotifications = [
+  {
+    id: 4,
+    timestamp: new Timestamp().subtract(1, "DAY"),
+    body: "Important! Please change your password.",
+    read: false,
+  },
+  {
+    id: 1,
+    timestamp: new Timestamp().subtract(4, "DAY"),
+    body: "Welcome, here's some things to get you started.",
+    read: false,
+  },
+  {
+    id: 3,
+    timestamp: new Timestamp().subtract(2, "DAY"),
+    body: "RJ has sent you a message.",
+    read: false,
+  },
+  {
+    id: 2,
+    timestamp: new Timestamp().subtract(3, "DAY"),
+    body: "Upload your profile image.",
+    read: false,
+  },
+];
--- js/utils.js
+++ js/utils.js
@@ -2,14 +2,38 @@ function sleep(seconds = 0) {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve();
-    }, time * 1000);
+    }, seconds * 1000);
   });
 }
 
-function randomInt(max, type = "int") {
+function randomNumber(max, type = "int") {
   if (type === "int") {
     return Math.floor(Math.random() * max);
   } else {
     return Math.random() * max;
   }
 }
+
+/* timestamps */
+function Timestamp() {
+  this.value = Math.floor(Date.now() / 1000);
+}
+
+Timestamp.prototype = {
+  units: {
+    DAY: 86400,
+    HOUR: 3600,
+  },
+
+  add: function (number, unit) {
+    const _unit = this.units[unit];
+    this.value = this.value + Number(number) * _unit;
+    return this.value;
+  },
+
+  subtract: function (number, unit) {
+    const _unit = this.units[unit];
+    this.value = this.value - Number(number) * _unit;
+    return this.value;
+  },
+};
