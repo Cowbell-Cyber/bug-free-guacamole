1642522b0bfe100b4d481c523c3b01794d9e1c66
Trevor Pierce <trevthewebdev@gmail.com>
2022-01-09 23:45:56 -0600

    different message types

--- js/database.js
+++ js/database.js
@@ -3,21 +3,25 @@ const initialNotifications = [
     id: 4,
     timestamp: new Timestamp().subtract(1, "DAY"),
     body: "Important! Please change your password.",
+    type: "alert",
   },
   {
     id: 1,
     timestamp: new Timestamp().subtract(4, "DAY"),
     body: "Welcome, here's some things to get you started.",
+    type: "info",
   },
   {
     id: 3,
     timestamp: new Timestamp().subtract(2, "DAY"),
     body: "RJ has sent you a message.",
+    type: "info",
   },
   {
     id: 2,
     timestamp: new Timestamp().subtract(3, "DAY"),
     body: "Upload your profile image.",
+    type: "info",
   },
 ];
 
