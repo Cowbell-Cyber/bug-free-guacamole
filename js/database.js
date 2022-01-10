a8776e8c6dfb0fb3bc71b33b18ce61990f77bcd6
Trevor Pierce <trevthewebdev@gmail.com>
2022-01-09 23:40:28 -0600

    test setup

--- js/database.js
+++ js/database.js
@@ -3,25 +3,21 @@ const initialNotifications = [
     id: 4,
     timestamp: new Timestamp().subtract(1, "DAY"),
     body: "Important! Please change your password.",
-    read: false,
   },
   {
     id: 1,
     timestamp: new Timestamp().subtract(4, "DAY"),
     body: "Welcome, here's some things to get you started.",
-    read: false,
   },
   {
     id: 3,
     timestamp: new Timestamp().subtract(2, "DAY"),
     body: "RJ has sent you a message.",
-    read: false,
   },
   {
     id: 2,
     timestamp: new Timestamp().subtract(3, "DAY"),
     body: "Upload your profile image.",
-    read: false,
   },
 ];
 
